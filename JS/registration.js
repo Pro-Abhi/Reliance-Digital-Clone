e016ac3b4adb63d7baeb45e88006abb2c715e721
Abhishek <abhishekkolapkar10@gmail.com>
2022-09-06 08:07:20 +0530

    User Registration Page

--- JS/registration.js
+++ JS/registration.js
@@ -1,21 +1,3 @@
-// sticky header
-let body = document.querySelector('BODY')
-let header = document.querySelector('.header-linkinfo')
-let sticky_header = document.querySelector('.header')
-
-let observer = new IntersectionObserver((entries) => {
-  let ent = entries[0]
-  // console.log(ent);
-  ent.isIntersecting === false ? body.classList.add('sticky') : body.classList.remove('sticky')
-}, {
-  root: null,
-  rootMargin: "",
-  threshold: 0,
-})
-
-observer.observe(header)
-
-
 // registration form validation and store data in localStorage
 
 // email validation
