a4ea8a3bc50be8567bfc40b44bd0aa78bce56a4e
Abhishek <abhishekkolapkar10@gmail.com>
2022-09-06 09:13:48 +0530

    Home Page

--- JS/script.js
+++ JS/script.js
@@ -65,15 +65,26 @@ inputScreen.addEventListener("focus", () => {
 });
 
 
-// slider-1 js
+// slider js 
 const slides = document.getElementsByClassName("slide");
-// console.log(slides);
+const slider__btns = document.querySelectorAll(".slider__navigation__btn");
+const slides2 = document.getElementsByClassName("vivo__galaxy__slide");
+
+
+// console.log(slider__btns);
 let flag = 0;
+let currentSlide = 1;
+slider__btns.forEach(element => {
+    // console.log(element);
+  });
+
 
 function controller(x){
     flag = flag + x;
     slidesShow(flag);
 }
+
+
 slidesShow(flag);
 
 function slidesShow(num){
@@ -86,8 +97,32 @@ function slidesShow(num){
         num = slides.length-1;
     }
     for(let y of slides){
-      console.log(y);
         y.style.display = "none";
-    }
+       }
     slides[num].style.display = "block";
+    
+}
+
+
+// slider-2
+let flag2 = 0;
+function controller2(y){
+    flag2 = flag2 + y;
+    slideShow2(flag2);
+}
+slideShow2(flag2);
+
+function slideShow2(num2){
+    if(num2 == slides2.length){
+        num2=0;
+        flag2=0;
+    }
+    if(num2 < 0){
+     num2=slides2.length-1;
+     flag2=slides2.length-1;
+    }
+    for(let y of slides2){
+        y.style.display = "none";
+       }
+    slides2[num2].style.display = "block";
 }
