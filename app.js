10dd825b188b0ce8cc3f70ce84b8db5683aeca80
Chandan Kumar <krc6893@gmail.com>
2022-09-07 13:25:21 +0530

    products slider 3

--- app.js
+++ app.js
@@ -62,27 +62,59 @@ function slideShow2(num2){
 
 // slider3 js 
 
-let flag3 = 0;
-function controller3(z){
-    flag3 = flag3 + z;
-    slideShow3(flag3);
-}
-slideShow3(flag3);
+// let flag3 = 0;
+// function controller3(z){
+//     flag3 = flag3 + z;
+//     slideShow3(flag3);
+// }
+// slideShow3(flag3);
 
-function slideShow3(num3){
-    if(num3 == slides3.length){
-        num3=0;
-        flag3=0;
-    }
-    if(num3 < 0){
-     num3=slides3.length-1;
-     flag3=slides3.length-1;
-    }
-    for(let y of slides3){
-        y.style.display = "none";
-       }
-    slides3[num3].style.display = "block";
+// function slideShow3(num3){
+//     if(num3 == slides3.length){
+//         num3=0;
+//         flag3=0;
+//     }
+//     if(num3 < 0){
+//      num3=slides3.length-1;
+//      flag3=slides3.length-1;
+//     }
+//     for(let y of slides3){
+//         y.style.display = "none";
+//        }
+//     slides3[num3].style.display = "block";
     
+// }
+
+var left=1;
+var right=5;
+
+function show(){
+
+for(i=left;i<=right;i++){
+   document.getElementById("c"+i).style.display="inline-block"
+//    console.log(document.getElementById("c"+i).style.display="inline-block");
+}
+}
+function moveLeft(){
+if(left<=3 && right<=7){
+    document.getElementById("c"+left).style.display="none";
+    left+=1;
+    right+=1;
+    for(i=left;i<=right;i++){
+        document.getElementById("c"+i).style.display="inline-block"
+    }
+}else return;
+}
+
+function moveRight(){
+if(left>=2 && right>=6){
+    document.getElementById("c"+right).style.display="none";
+    left-=1;
+    right-=1;
+    for(i=left;i<=right;i++){
+        document.getElementById("c"+i).style.display="inline-block"
+    }
+}else return;
 }
 
 
