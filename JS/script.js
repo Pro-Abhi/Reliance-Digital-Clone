3b75edb266a9f6e358adc6fa15f5185fbbe84efd
Abhishek <abhishekkolapkar10@gmail.com>
2022-09-08 11:01:07 +0530

    add slider on home page

--- JS/script.js
+++ JS/script.js
@@ -126,3 +126,37 @@ function slideShow2(num2){
        }
     slides2[num2].style.display = "block";
 }
+
+
+// slider-3
+var left=1;
+var right=5;
+
+function show(){
+
+for(i=left;i<=right;i++){
+   document.getElementById("c"+i).style.display="inline-block"
+//    console.log(document.getElementById("c"+i).style.display="inline-block");
+}
+}
+function moveLeft(){
+if(left<=3 && right<=7){
+    document.getElementById("c"+left).style.display="none";
+    left+=1;
+    right+=1;
+    for(i=left;i<=right;i++){
+        document.getElementById("c"+i).style.display="inline-block"
+    }
+}else return;
+}
+
+function moveRight(){
+if(left>=2 && right>=6){
+    document.getElementById("c"+right).style.display="none";
+    left-=1;
+    right-=1;
+    for(i=left;i<=right;i++){
+        document.getElementById("c"+i).style.display="inline-block"
+    }
+}else return;
+}
