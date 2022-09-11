43b93de0594503201d29dc55ce8900f1d1f36a69
puranchandralohar <clpuran95@gmail.com>
2022-09-11 08:04:54 +0530

    changes

--- JS/product_search.js
+++ JS/product_search.js
@@ -51,39 +51,37 @@ function addElement(value){
 
     let { images, title,price } = value;
 
-    let result = `<div class="item watch_item">
-    <a href="#">
-        <div class="product_image">
-            <img src="${images}" alt="${title}">
-        </div>
-        <div class="product_title title">${title}</div>
-        <div class="review-section">
-          <span class="stars">
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-regular fa-star"></i>
-          </span>
-          <span class="review-count">(10 reviews)</span>
-        </div>
-        <div class="price-section">
-          <div class="offer">
-            <span class="name">Offer Price: </span>
-            <span class="price big">₹${price}</span>
-          </div>
-          <div class="mrp">
-            <span class="name">M.R.P: </span>
-            <span class="price strike">₹${price+600}</span>
-          </div>
-          <div class="saving-detail">
-            <span class="name">You Save: </span>
-          </div>
-        </div>
-        <div class="offer-available-tag">Offer available</div>
-    </a>
-</div>`;
-productContainer.innerHTML += result;
+    let result = `<div class="item each_item">
+                    <a href="#">
+                        <div class="product_image">
+                            <img src="${images}" alt="${title}">
+                        </div>
+                        <div class="product_title title">${title}</div>
+                        <div class="review-section">
+                        <span class="stars">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                        </span>
+                        <span class="review-count">(10 reviews)</span>
+                        </div>
+                        <div class="price-section">
+                        <div class="offer">
+                            <span class="name">Offer Price: </span>
+                            <span class="price big">₹${price}</span>
+                        </div>
+                        <div class="mrp">
+                            <span class="name">M.R.P: </span>
+                            <span class="price strike">₹${price+600}</span>
+                        </div>
+
+                        </div>
+                        <div class="offer-available-tag">Offer available</div>
+                    </a>
+                </div>`;
+            productContainer.innerHTML += result;
 }
 
 
